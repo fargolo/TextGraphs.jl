08a3af4e921c1c140d4eef521614cf00f69b8131
fargolo <felipe.c.argolo@protonmail.com>
2022-12-05 17:46:30 -0300

    window props working. To do: get average values from prop_array

--- dev/window-props.jl
+++ dev/window-props.jl
@@ -6,11 +6,12 @@ Calculate average properties from subsets of text.
 function window_props(nwindow,graph_function,raw_text)
 
     tokenized_words = WordTokenizers.punctuation_space_tokenize(lowercase(raw_text))
-    text_array = [tokenized_words[i:(i+nwindow)] for i in 1:(length(tokenized_words) - nwindow)]
-    
+    text_arrays = [tokenized_words[i:(i+nwindow-1)] for i in 1:(length(tokenized_words) - nwindow+1)]
+    text_array = map(x->join(x," "),text_arrays)
     graph_array = map(graph_function,text_array)
+    
     prop_array = map(graph_props,graph_array)
 
-    # Get average values
+    # Get average values from array of dicts.
 
 end
