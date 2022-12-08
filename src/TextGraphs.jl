c28c16d80ea48a8e579c07b4d8567c333d002421
fargolo <felipe.c.argolo@protonmail.com>
2022-12-08 15:04:25 -0300

    small typo in export (missing ending comma

--- src/TextGraphs.jl
+++ src/TextGraphs.jl
@@ -9,7 +9,7 @@ using DataFrames
 export 
 	naive_graph, phrases_graph,
 	stem_graph, lemma_graph, pos_graph,
-	node_props, graph_props, window_props, rand_erdos_ratio_props, rand_erdos_ratio_sampled , get_graph_labels
+	node_props, graph_props, window_props, rand_erdos_ratio_props, rand_erdos_ratio_sampled , get_graph_labels ,
 	latent_space_graph, latent_space_dists, get_embedding
 
 include("basic_tools.jl")
