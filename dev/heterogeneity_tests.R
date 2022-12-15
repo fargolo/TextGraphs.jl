ebd492b22cfd2bc10967cc38184f9869232d29d2
fargolo <felipe.c.argolo@protonmail.com>
2022-12-15 19:50:59 -0300

    Played around a little to find a transformation of det(X) that scales linearly with length(X)

--- dev/heterogeneity_tests.R
+++ dev/heterogeneity_tests.R
@@ -1,3 +1,7 @@
+library(igraph)
+library(extraDistr)
+library(purrr)
+
 shannon = function(p) {
   x = p * log2(p)
   x = replace(x, is.nan(x), 0)
@@ -32,18 +36,24 @@ heterogeneity = function(g, D, mode = "col") {
               rao = apply(A, dim, rao, D)))
 }
 
-library(igraph)
 
 mat_dim = 5
 full_adj = matrix(data=rep(1,mat_dim**2),nrow=mat_dim,ncol=mat_dim)
 adj_mat = matrix(data=rbinom(n = mat_dim**2,size = 1,prob=.5),nrow=mat_dim,ncol=mat_dim)
 rnd_mat = matrix(data=rnorm(n = mat_dim**2,sd = 15),nrow=mat_dim,ncol=mat_dim)
+rnd_mat_array = purrr::map(.f = function(x) matrix(data=extraDistr::rhnorm(n = x**2),nrow=x,ncol=x),.x=c(1:30))
+rnd_mat_array = purrr::map(.f = function(x) matrix(data=rnorm(n = x**2),nrow=x,ncol=x),.x=c(1:30))
 
-random_g <- graph_from_adjacency_matrix(full_adj)
+random_g <- graph_from_adjacency_matrix(adj_mat)
 g_attr <- set_edge_attr(random_g,"weight", value = sum(adj_mat))
 
+# Found a transformation for det(x) that scales linearly with length(x)
+## Log-det uhhraay :) 
+## f(det(x)) = log(abs(det(x))) / length(x)
 heterogeneity(g_attr,D=rnd_mat,mode="col")
-det(rnd_mat)
+scaled_determs <- purrr::map(rnd_mat_array,.f=function(x) log(abs(det(x))) / length(x))
+abs_determs <- purrr::map(scaled_determs,abs)
+plot(c(1:length(determs)),scaled_determs)
 #Inspecting Rao's objects
 #AB = as_adjacency_matrix(g_attr, attr = "weight", sparse = FALSE)
 #Proportion of total edges ("evenness") for each node: AC = diag(1/rowSums(AB)) %*% AB 
